6cebf0e148a53aac6ef2bf2e8d53ccd0f3ab8482
taki199 <christotaki22@gmail.com>
2024-12-04 18:18:51 +0100

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './components/Features'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
      <Header/>
+     <Hero/>
+     <Features/>
     </main>
   )
 }
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -27,9 +27,14 @@ const [isOpen,setIsOpen]=useState(false)
                   <NavLink title="pricing" />
                 </li>
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll to="hero"
+                              offset={-100}
+                              spy
+                              smooth
+                              className={clsx('max-lg:hidden transition-transform duration-500 cursor-pointer')}
+                  >
                     <img
-                      src="/images/xora.svg"
+                      src="/images/xora.svg"    
                       width={160}
                       height={55}
                       alt="logo"
@@ -43,6 +48,10 @@ const [isOpen,setIsOpen]=useState(false)
                 </li>
               </ul>
             </nav>
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                <img src="/images/bg-outlines.svg"width={960} height={380}  alt="outline" className="relative z-2" />
+                <img src="/images/bg-outlines-fill.png"width={960} height={380}  alt="outline" className="absolute inset-0 mix-blend-soft-light opacity-5" />
+            </div>
           </div>
         </div>
         
